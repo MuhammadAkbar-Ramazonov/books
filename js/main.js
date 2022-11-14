4b0d1883cc9214160460f17b79cf74a0eb4321d1
John Doe <ramazonov3600@gmail.com>
2022-11-14 17:57:53 +0500

    add search with sort

--- js/main.js
+++ js/main.js
@@ -6,9 +6,8 @@ const elSelectAuthor = elForm.querySelector(".site-select-author");
 const elSelectLanguage = elForm.querySelector(".site-select-language");
 const elList = document.querySelector(".books-list");
 
-
+// feragment
 const newFragment = document.createDocumentFragment();
-// template
 
 function showSearchMovies(search) {
   const filteredMovies = books.filter(item => {
@@ -18,24 +17,68 @@ function showSearchMovies(search) {
   return filteredMovies;
 };
 
+function sortArr(arr, select){
+  if(select == "a-z"){
+    arr.sort((a, b) => {
+      if (String(a.title) > String(b.title)) {
+        return -1
+      }
+      if (String(a.title) < String(b.title)) {
+        return 1
+      }
+      return 0
+    })
+  } else if(select == "z-a"){
+    arr.sort((a, b) => {
+      if (String(a.title) < String(b.title)) {
+        return -1
+      }
+      if (String(a.title) > String(b.title)) {
+        return 1
+      }
+      return 0
+    })
+  }
+  if(select == "new-to-old"){
+    arr.sort((a, b) => a.year - b.year)
+  }
+  else if(select == "old-to-new"){
+    arr.sort((a, b) => b.year - a.year)
+  }
+  
+  if(select == "page-from-little"){
+    arr.sort((a, b)=> a.pages - b.pages)
+  }
+  else if(select == "page-from-alot"){
+    arr.sort((a, b)=> b.pages - a.pages)
+  }
+}
 
-for (const  book of books) {
-
-  const elTemp = document.querySelector(".book-temp").content;
-  const elCloneTemp = elTemp.cloneNode(true);
-
-  elCloneTemp.querySelector(".book-img").src = book.imageLink;
-  elCloneTemp.querySelector(".book-title").textContent = book.title;
-  elCloneTemp.querySelector(".book-desc").textContent = book.author;
-  elCloneTemp.querySelector(".book-year").textContent = book.year;
-  elCloneTemp.querySelector(".book-page").textContent = book.pages;
-  elCloneTemp.querySelector(".book-language").textContent = book.language;
-  elCloneTemp.querySelector(".book-link").href = book.link;
-
-  newFragment.appendChild(elCloneTemp);
-};
-
-elList.appendChild(newFragment);
+function renderArr(arr, regex = ""){
+  for (const  book of arr) {
+    
+    const elTemp = document.querySelector(".book-temp").content;
+    const elCloneTemp = elTemp.cloneNode(true);
+    
+    elCloneTemp.querySelector(".book-img").src = book.imageLink;
+    
+    if(regex.source != "(?:)" && regex){
+      elCloneTemp.querySelector(".book-title").innerHTML = book.title.replace(regex, `<mark class="bg-warning">${regex.source.toLowerCase()}</mark>`);
+    } else {
+      elCloneTemp.querySelector(".book-title").textContent = book.title;
+    }
+    elCloneTemp.querySelector(".book-desc").textContent = book.author;
+    elCloneTemp.querySelector(".book-year").textContent = book.year;
+    elCloneTemp.querySelector(".book-page").textContent = book.pages;
+    elCloneTemp.querySelector(".book-language").textContent = book.language;
+    elCloneTemp.querySelector(".book-link").href = book.link;
+    
+    newFragment.appendChild(elCloneTemp);
+  };
+  
+  elList.appendChild(newFragment);
+  
+}
 
 elForm.addEventListener("submit", evt =>{
   evt.preventDefault();
@@ -43,39 +86,32 @@ elForm.addEventListener("submit", evt =>{
   elList.innerHTML = "";
   
   const elFormInputValue = elSearchInput.value.trim();
-  const regexValue = new RegExp(elFormInputValue, "gi");
+  const elSearchSortValue = elSearchSort.value.trim();
   
-  const searchMovie = showSearchMovies(regexValue)
   
+  const regex = new RegExp(elFormInputValue, "gi");
+  
+  const searchMovie = showSearchMovies(regex)
   
   if (searchMovie.length > 0){
-    
-    for (const  book of searchMovie) {
-      
-      const elTemp = document.querySelector(".book-temp").content;
-      const elCloneTemp = elTemp.cloneNode(true);
-      
-      elCloneTemp.querySelector(".book-img").src = book.imageLink;
-      
-      if(regexValue.source != "(?:)" && regexValue){
-        elCloneTemp.querySelector(".book-title").innerHTML = book.title.replace(regexValue, `<mark class="bg-warning">${regexValue.source.toLowerCase()}</mark>`);
-      } else {
-        elCloneTemp.querySelector(".book-title").textContent = book.title;
-      }
-      elCloneTemp.querySelector(".book-desc").textContent = book.author;
-      elCloneTemp.querySelector(".book-year").textContent = book.year;
-      elCloneTemp.querySelector(".book-page").textContent = book.pages;
-      elCloneTemp.querySelector(".book-language").textContent = book.language;
-      elCloneTemp.querySelector(".book-link").href = book.link;
-      
-      newFragment.appendChild(elCloneTemp);
-    };
-    
-    elList.appendChild(newFragment);
-    
-    
+    sortArr(searchMovie, elSearchSortValue)
+    renderArr(searchMovie, regex);
   }else{
     elList.innerHTML = "Movie not found !!!";
   }
-})
+});
 
+
+const err = ["dfssd", "sdfsdf", "sadasd","asdad"]
+
+err.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  }
+  if (a < b) {
+    return -1;
+  }
+  return 0;
+});
+
+renderArr(books);
