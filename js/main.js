0146a2eb04cfe6847f78af233ddec117c06cce63
John Doe <ramazonov3600@gmail.com>
2022-11-14 12:05:22 +0500

    add search with input

--- js/main.js
+++ js/main.js
@@ -9,20 +9,71 @@ const elList = document.querySelector(".books-list");
 
 const newFragment = document.createDocumentFragment();
 // template
-const elTemp = document.querySelector(".book-temp").content;
 
-for (const  book of books) {
-
-  const elCloneTemp = elTemp.cloneNode(true);
-
-  elCloneTemp.querySelector(".book-img").src = book.imageLink;
-  elCloneTemp.querySelector(".book-title").textContent = book.title;
-  elCloneTemp.querySelector(".book-desc").textContent = book.author;
-  elCloneTemp.querySelector(".book-year").textContent = book.year;
-  elCloneTemp.querySelector(".book-page").textContent = book.pages;
-  elCloneTemp.querySelector(".book-language").textContent = book.language;
-  elCloneTemp.querySelector(".book-link").href = book.link;
-
-  newFragment.appendChild(elCloneTemp);
+function showSearchMovies(search) {
+  const filteredMovies = books.filter(item => {
+    const moreCriterias =  String(item.title).match(search);
+    return moreCriterias;
+  });  
+  return filteredMovies;
 };
-elList.appendChild(newFragment);
+
+elForm.addEventListener("submit", evt =>{
+  evt.preventDefault();
+
+  elList.innerHTML = "";
+  
+  const elFormInputValue = elSearchInput.value.trim();
+  const regexValue = new RegExp(elFormInputValue, "gi");
+  
+  const searchMovie = showSearchMovies(regexValue)
+  
+  if (searchMovie.length > 0){
+    
+    for (const  book of searchMovie) {
+      
+      const elTemp = document.querySelector(".book-temp").content;
+      const elCloneTemp = elTemp.cloneNode(true);
+      
+      elCloneTemp.querySelector(".book-img").src = book.imageLink;
+      
+      if(regexValue.source != "(?:)" && regexValue){
+        elCloneTemp.querySelector(".book-title").innerHTML = book.title.replace(regexValue, `<mark class="bg-warning">${regexValue.source.toLowerCase()}</mark>`);
+      } else {
+        elCloneTemp.querySelector(".book-title").textContent = book.title;
+      }
+      elCloneTemp.querySelector(".book-desc").textContent = book.author;
+      elCloneTemp.querySelector(".book-year").textContent = book.year;
+      elCloneTemp.querySelector(".book-page").textContent = book.pages;
+      elCloneTemp.querySelector(".book-language").textContent = book.language;
+      elCloneTemp.querySelector(".book-link").href = book.link;
+      
+      newFragment.appendChild(elCloneTemp);
+    };
+    
+    elList.appendChild(newFragment);
+    
+    
+  }else{
+    elList.innerHTML = "Movie not found !!!";
+  }
+})
+
+// for (const  book of books) {
+  
+//   const elTemp = document.querySelector(".book-temp").content;
+//   const elCloneTemp = elTemp.cloneNode(true);
+  
+//   elCloneTemp.querySelector(".book-img").src = book.imageLink;
+//   elCloneTemp.querySelector(".book-title").textContent = book.title;
+//   elCloneTemp.querySelector(".book-desc").textContent = book.author;
+//   elCloneTemp.querySelector(".book-year").textContent = book.year;
+//   elCloneTemp.querySelector(".book-page").textContent = book.pages;
+//   elCloneTemp.querySelector(".book-language").textContent = book.language;
+//   elCloneTemp.querySelector(".book-link").href = book.link;
+  
+//   newFragment.appendChild(elCloneTemp);
+// };
+
+// elList.appendChild(newFragment);
+
